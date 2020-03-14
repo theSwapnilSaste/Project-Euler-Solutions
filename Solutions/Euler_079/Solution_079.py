7bfa879cf677a8c890389ba924721b726b45e76a
theSwapnilSaste <theSwapnilSaste@gmail.com>
2020-03-14 15:35:46 +0530

    #001 [ Files Created ] : Blank Folder and empty solution files
    Created 9 folders and 5 files for solution  in different language using Shell Script.

---
+++ Solutions/Euler_079/Solution_079.py
@@ -0,0 +1,10 @@
+#  Replace this file with the Solution for Project Euler Problem #079
+#  Project Euler Problem: #079
+#  Repository Maintainer: https://www.github.com/theSwapnilSaste
+#  File Creation Date   : 14th March 2020
+#  Solution Author      : **** Insert Your Name/Github Handle Here ***
+#  Solution added on    : **** Insert Date here ****
+#  Problem Status       : Complete/Incomplete/Need Improvement etc. 
+#  Space for Notes
+#  .
+#  .
