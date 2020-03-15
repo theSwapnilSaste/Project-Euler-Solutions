171170a1ede06e0be932048598ff9855e4f9c822
theSwapnilSaste <theSwapnilSaste@gmail.com>
2020-03-15 13:09:17 +0530

    #1  Java Solution for Problem 1 Added

--- Solutions/Euler_001/Solution_001.java
+++ Solutions/Euler_001/Solution_001.java
@@ -2,9 +2,28 @@
 //  Project Euler Problem: #001
 //  Repository Maintainer: https://www.github.com/theSwapnilSaste
 //  File Creation Date   : 14th March 2020
-//  Solution Author      : **** Insert Your Name/Github Handle Here ***
-//  Solution added on    : **** Insert Date here ****
-//  Problem Status       : Complete/Incomplete/Need Improvement etc. 
-//  Space for Notes
+//  Solution Author      : Swapnil Saste | https://www.github.com/theSwapnilSaste
+//  Solution added on    : 14th March 2020
+//  Problem Status       : COMPLETED WITHOUT ERROR 
+//  Space for Notes      : Sum of Arithmetic Progression is used to reduce Execution Time
 //  .
 //  .
+public class Solution_001 {
+	public static void main(String[] args) {
+		// long time = System.nanoTime();
+		int l = 3, j = 5, range = 999;
+		System.out.println(SumPartial(l, range) + SumPartial(j, range) - SumPartial(15, range));
+		// System.out.println( "time used = " + (System.nanoTime() - time));
+	}
+	public static int maxInRange(int numb, int rangeLimit) {
+		int x = (Integer) (rangeLimit / numb);
+		return x * numb;
+	}
+	public static int SumPartial(int i, int range) {
+		int max = maxInRange(i, range);
+		return (max * (i + max) / (2 * i));
+	}
+}
+// OUTPUT:
+// 233168 
+// time used=207,917 nanoseconds
